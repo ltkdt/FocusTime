bfb0033ab29b7dab9780c23930178abe80a75396
ks5-dev <kienlt2306@gmail.com>
2022-03-20 00:06:59 +0700

    Testing

--- todo.py
+++ todo.py
@@ -100,7 +100,7 @@ class Todo(QWidget):
         if self.openAddTask is None:
             self.openAddTask = AddTaskWindow()
         self.openAddTask.show()
-
+# Checking github actions :))
 '''
 app = QApplication(sys.argv)
 window = Window()
